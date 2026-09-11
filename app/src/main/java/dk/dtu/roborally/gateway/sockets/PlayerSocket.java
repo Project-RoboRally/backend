9107403606b89c623044bc457fc17f4ef0c2e314
Elias <102224705+bondegaard@users.noreply.github.com>
2026-09-11 16:51:41 +0200

    Added basic structure for zmq/websockets

---
+++ app/src/main/java/dk/dtu/roborally/gateway/sockets/PlayerSocket.java
@@ -0,0 +1,17 @@
+package dk.dtu.roborally.gateway.sockets;
+
+import org.zeromq.ZMQ;
+
+/**
+ * Socket for the player to send messages from the player to the server
+ *
+ * @author Elias
+ */
+public class PlayerSocket extends AbstractSocket {
+    public PlayerSocket(ZMQ.Socket socket) {
+        super(socket);
+    }
+
+    // todo: Setup zmq and setup a reciever where when a message is recieved,
+    //  it is matched with a specific player and the message is send to the logic.
+}
