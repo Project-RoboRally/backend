bdd8414227309de722b850e81073bcccf44b4ecf
Nicoleta <nicoleta@MacBookPro.localdomain>
2026-09-16 20:48:04 +0200

    removed all options, they are in by default

--- app/src/main/java/dk/dtu/roborally/config/CorsConfig.java
+++ app/src/main/java/dk/dtu/roborally/config/CorsConfig.java
@@ -15,7 +15,6 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOriginPatterns(allowedOriginPatterns)
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
