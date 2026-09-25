80e174b63d55325936b4a207732267f160fa3414
Victor <34196360+slayervictor@users.noreply.github.com>
2026-09-25 16:21:34 +0200

    comments

--- app/src/test/java/dk/dtu/roborally/loaders/LevelLoaderTest.java
+++ app/src/test/java/dk/dtu/roborally/loaders/LevelLoaderTest.java
@@ -12,12 +12,15 @@ import dk.dtu.roborally.models.Board;
 class LevelLoaderTest {
 
     @Test
+    // Test to see if the loaders crash if the optional level objects are missing
     void loadAllowsMissingOptionalLevelObjects() {
         Board board = assertDoesNotThrow(() -> LevelLoader.load("levels/level_without_optional_objects.json"));
         assertNotNull(board);
     }
 
     @Test
+    // Check to see if the level loader throws an error when there are no squares.
+    // Cause squares are an requirement
     void loadRejectsMissingSquares() {
         IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
                 () -> LevelLoader.load("levels/level_without_squares.json"),
